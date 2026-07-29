971c790e73cebf924edc0702dc2684bb4cccb9de
Sara Abdullahi <sara.abdullahi.22@ucl.ac.uk>
2026-07-29 16:45:20 +0100

    Initial Commit

---
+++ cursive/cursiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursiveApp.swift
+//  cursive
+//
+//  Created by Sara Abdullahi on 29/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct cursiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cursiveTests/cursiveTests.swift
@@ -0,0 +1,19 @@
+//
+//  cursiveTests.swift
+//  cursiveTests
+//
+//  Created by Sara Abdullahi on 29/07/2026.
+//
+
+import Testing
+@testable import cursive
+
+struct cursiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
